4550ebdb0a032956448a7929ac9a1e690394465f
madvas <matus.lestan@gmail.com>
2017-02-21 19:56:20 +0100

    update to cljs-react-material-ui "0.2.38"

--- project.clj
+++ project.clj
@@ -19,8 +19,8 @@
                  [juxt.modular/wire-up "0.5.0"]
                  [juxt.modular/aleph "0.1.4"]
                  [com.andrewmcveigh/cljs-time "0.4.0"]
-                 [cljs-react-material-ui "0.2.37"]
-                 [cljsjs/material-ui-chip-input "0.11.2-0"]
+                 [cljs-react-material-ui "0.2.38"]
+                 [cljsjs/material-ui-chip-input "0.13.0-0"]
                  [prismatic/schema "1.1.1"]
                  [print-foo-cljs "2.0.0"]]
 
